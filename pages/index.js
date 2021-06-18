6182d1d89e33f892ffc9cddd372b210eb425090f
pranaytejareddy1996 <ch.pranaytejareddy@gmail.com>
2021-06-18 17:01:59 +0530

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -39,9 +39,9 @@ export default function Home() {
           <p className='link'>Images</p>
           <p className='link'>Gmail</p>
 
-          <ViewGridIcon className='h-10 w-10 p-2 rounded-full 
+          {/* <ViewGridIcon className='h-10 w-10 p-2 rounded-full 
           hover:bg-gray-100 cursor-pointer'/>
-          <Avatar url='https://coaching.papareact.com/ai9'/>
+          <Avatar url='https://coaching.papareact.com/ai9'/> */}
         </div>
         
       </header>
