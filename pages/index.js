68634d79398675ca6a00230d9dbe5491b7a01082
pranaytejareddy1996 <ch.pranaytejareddy@gmail.com>
2021-06-18 17:15:52 +0530

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,18 @@ import { useRef } from 'react'
 import {useRouter} from 'next/router';
 
 export default function Home() {
-  // const router = useRouter();
-  // const searchInputRef = useRef(null);
+  const router = useRouter();
+  const searchInputRef = useRef(null);
 
-  // const search = (e) => {
-  //   e.preventDefault();
-  //   const term = searchInputRef.current.value;
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
 
-  //   if(!term) return;
+    if(!term) return;
 
-  //   router.push(`/search?term=${term}`);
+    router.push(`/search?term=${term}`);
 
-  // };
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen ">
@@ -39,14 +39,41 @@ export default function Home() {
           <p className='link'>Images</p>
           <p className='link'>Gmail</p>
 
-          {/* <ViewGridIcon className='h-10 w-10 p-2 rounded-full 
+          <ViewGridIcon className='h-10 w-10 p-2 rounded-full 
           hover:bg-gray-100 cursor-pointer'/>
-          <Avatar url='https://coaching.papareact.com/ai9'/> */}
+          <Avatar url='https://coaching.papareact.com/ai9'/>
         </div>
         
       </header>
 
-      
+      <form className='flex flex-col items-center mt-44 flex-grow w-4/5'>
+        <Image src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+        width={300}
+        height={100} />
+
+        <div className='flex w-full mt-3 hover:shadow-lg
+        focus-within:shadow-lg max-w-md rounded-full border
+        border-gray-200 px-5 py-3 items-center sm:max-w-xl 
+        lg:max-w-2xl '>
+          <SearchIcon className='h-5 mr-3 text-gray-500'/>
+          <input 
+            ref={searchInputRef}
+            type='text' 
+            className=' flex-grow focus:outline-none'  />
+          <MicrophoneIcon className='h-5'/>
+        </div>
+
+        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 
+        sm:space-y-0 sm:flex-row sm:space-x-4 ' >
+          <button className='btn' onClick={search}>Google Search</button>
+
+          <button className='btn' onClick={search}>I'm Feeling Lucky</button>
+
+        </div>
+      </form>
+
+      {/* footer */}
+      <Footer />
      
     </div>
   )
