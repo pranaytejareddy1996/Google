fd7af227624c53053e0b608b5602672e802a1bcc
pranaytejareddy1996 <ch.pranaytejareddy@gmail.com>
2021-06-18 17:48:30 +0530

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,9 @@ export default function Home() {
 
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 
         sm:space-y-0 sm:flex-row sm:space-x-4 ' >
-          
+          <button className='btn'hidden type='submit' onClick={search}></button>
+
+          <button className='btn' onClick={search}></button>
 
         </div>
       </form>
