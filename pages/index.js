e4750cbe8940d48acda2497af8e1d9273db30ac2
pranaytejareddy1996 <ch.pranaytejareddy@gmail.com>
2021-06-18 16:59:58 +0530

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -46,34 +46,7 @@ export default function Home() {
         
       </header>
 
-      <form className='flex flex-col items-center mt-44 flex-grow w-4/5'>
-        <Image src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-        width={300}
-        height={100} />
-
-        <div className='flex w-full mt-3 hover:shadow-lg
-        focus-within:shadow-lg max-w-md rounded-full border
-        border-gray-200 px-5 py-3 items-center sm:max-w-xl 
-        lg:max-w-2xl '>
-          <SearchIcon className='h-5 mr-3 text-gray-500'/>
-          <input 
-            
-            type='text' 
-            className=' flex-grow focus:outline-none'  />
-          <MicrophoneIcon className='h-5'/>
-        </div>
-
-        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 
-        sm:space-y-0 sm:flex-row sm:space-x-4 ' >
-          <button className='btn' >Google Search</button>
-
-          <button className='btn' >I'm Feeling Lucky</button>
-
-        </div>
-      </form>
-
-      {/* footer */}
-      <Footer />
+      
      
     </div>
   )
