ea21d9a21457127eca59651dc273aab2f1eb988f
pranaytejareddy1996 <ch.pranaytejareddy@gmail.com>
2021-06-18 16:42:40 +0530

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -65,9 +65,9 @@ export default function Home() {
 
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 
         sm:space-y-0 sm:flex-row sm:space-x-4 ' >
-          <button className='btn' onClick={search}>Google Search</button>
+          <button className='btn' >Google Search</button>
 
-          <button className='btn' onClick={search}>I'm Feeling Lucky</button>
+          <button className='btn' >I'm Feeling Lucky</button>
 
         </div>
       </form>
